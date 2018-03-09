165e476c2174d4fd7e490d4a0e0294093fbab80a
Lucas JAHIER <lucasjahier@protonmail.com>
2018-03-09 23:30:08 +0100

    add security with token user generate when the register

--- routes/blog.js
+++ routes/blog.js
@@ -1,28 +1,44 @@
 module.exports = (app, pool, mysql, sha256, date, bodyParser) => {
+    let errors = {}
+    let outResults = {}
+
     app.param('id', function(req, res, next, id) {
         req.id = id
         next()
     })
 
-    // function verifUserToken(req) {
-    //     pool.query('SELECT token FROM users', function (error, results) {
-    //         if (error) throw error;
-    //         console.log(results)
-    //         let tokenHeader = req.header('Authorization')
-    //         if (results == tokenHeader) {
-    //             res.send("GROS PROUT")
-    //         }
-    //     })
-    // }
-
     // Just redirecting to homepage
     app.get('/blog', (req, res) => {
-        pool.query('SELECT * FROM posts', function (error, results) {
-            if (error) throw error;
-            if (results <= 2) {
-                res.end('There is no post in database')
+        let tokenHeader = req.header('Authorization')
+
+        res.header('Content-Type', 'application/json');
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+        pool.query("SELECT token FROM `users`", (error, results) => {
+            if (error) throw error
+            for (let i = 0; i < results.length; i++) {
+                let tokenResults = 'Bearer ' + results[i].token
+                if (tokenHeader === tokenResults) {
+                    errors = {}
+                    break
+                } else {
+                    errors.token = ['You are not connected !']
+                }
+            }
+
+            if (Object.values(errors).length > 0) {
+                res.send(errors)
             } else {
-                res.send(results)
+                pool.query('SELECT * FROM posts', function (error, results) {
+                    if (error) throw error;
+                    if (results <= 2) {
+                        errors.posts = "No post find in database"
+                        res.send(errors)
+                    } else {
+                        res.send(results)
+                    }
+                })
             }
         })
     })
@@ -32,9 +48,6 @@ module.exports = (app, pool, mysql, sha256, date, bodyParser) => {
         res.setHeader('Content-Type', 'application/json');
         res.header("Access-Control-Allow-Origin", "*");
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-
-        // let auth = req.header('Authorization')
-        // if ()
 
         let post_id = req.id
 
@@ -50,11 +63,18 @@ module.exports = (app, pool, mysql, sha256, date, bodyParser) => {
 
     // Post article into database
     app.post('/blog', (req, res) => {
+        // verifUserToken(req)
         res.setHeader('Content-Type', 'application/json')
         res.header("Access-Control-Allow-Origin", "*")
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Authorization")
 
-        let post = {title: req.body.title, content: req.body.content, author: req.body.author, created_at: date, updated_at: date}
+        let post = {
+            title: req.body.title,
+            content: req.body.content,
+            author: req.body.author,
+            created_at: date,
+            updated_at: date
+        }
 
         try {
             pool.query('INSERT INTO posts SET ?', post, function (error, results) {
@@ -72,11 +92,10 @@ module.exports = (app, pool, mysql, sha256, date, bodyParser) => {
         res.header("Access-Control-Allow-Origin", "*");
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 
-        // Authorisation et securisation des requetes faites à l'API
-        // console.log(req.header("Authorization"))
-
-        let put = {updated_at: date}
         let put_id =  req.id
+        let put = {
+            updated_at: date
+        }
 
         if (req.body.title != undefined)
             put.title = req.body.title
@@ -101,10 +120,10 @@ module.exports = (app, pool, mysql, sha256, date, bodyParser) => {
         res.header("Access-Control-Allow-Origin", "*");
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 
-        let put_id =  req.id
+        let post_id =  req.id
 
         try {
-            pool.query('DELETE FROM posts WHERE id = ?', put_id, function (error, results) {
+            pool.query('DELETE FROM posts WHERE id = ?', post_id, function (error, results) {
                 if (error) throw error;
                 res.redirect('/')
             })
